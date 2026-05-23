a6fcb83e449e83b9bedf0e80cde5569be5257b2b
Stefanie Guyer <guyerste@students.zhaw.ch>
2026-05-23 11:58:52 +0200

    verstehen der Daten

--- preprocessing.R
+++ preprocessing.R
@@ -1,10 +1,10 @@
 
-library(sf)
-
 library("sf")
+library("tidyverse")
 library("dplyr")
 library("purrr")
-library(stringr)
+library("stringr")
+library("tmap")
 
 # Eine liste aller .gpkg Dateien im Ordner erstellen:
 gpkg_files <- list.files("GPS Path data/", pattern = "\\.gpkg$", full.names = TRUE)
@@ -30,3 +30,73 @@ f_files <- function(file) {
 }
 
 alle_daten <- map_dfr(gpkg_files, f_files)
+
+###############################################################################
+# Datenstruktur 
+# 
+alle_daten
+
+alle_daten <- alle_daten |>
+  mutate(
+    Time = as.POSIXct(Time, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+  )
+
+###############################################################################
+# Spalten hinzufügen: RASSE 
+
+alle_daten <- alle_daten |>
+  mutate(
+    Rasse = stringr::str_extract(quelle_file, "(?<=-)[A-Z]{2}")
+  )
+
+###############################################################################
+
+# Daten verstehen: 
+View(alle_daten)
+
+# Weg aufzeichnen von einer Kuh am 25.6. am Abend: 
+
+R1_HO01_06_25_A <-  alle_daten |>
+  filter(quelle_file == "R1-HO01.gpkg",
+         TimeSlice == "06-25-A")
+
+R1_HO01_06_25_A |> 
+  ggplot() + 
+  geom_sf()
+
+# mit tmap 
+
+tmap_mode("view")
+
+tm_shape(R1_HO01_06_25_A) + 
+  tm_dots()
+
+# Herausfinden ob die Tiere der verschiedenen Rassen zusammen zum Melkstand getrieben werden oder separat. 
+
+View(alle_daten)
+
+Filter_1 <- alle_daten |>
+  filter(Time >= ymd_hms("2025-06-25 15:35:00"),
+         Time <= ymd_hms("2025-06-25 15:37:00")) 
+  
+Filter_1 |>
+  ggplot() +
+  geom_sf(aes(color = Rasse))
+
+View(Filter_1)
+
+# Distanz berechnen zu einem definierten Referenzpuntk: 
+
+ref_punkt <- st_sfc(st_point(c(2780425, 1161875)), crs = 2056)
+
+Filter_1 <- Filter_1 |> 
+  mutate(dist_to_ref = as.numeric(st_distance(geom, ref_punkt)))
+
+View(Filter_1)
+
+Filter_1 |> 
+  ggplot(aes(Rasse, dist_to_ref)) + 
+  geom_boxplot()
+
+
+
